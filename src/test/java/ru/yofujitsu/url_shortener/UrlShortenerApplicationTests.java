f81a5be8de6f64d39bb1fbd15a5108651f243211
yofujitsu <yofujitsuuu@gmail.com>
2025-11-09 16:10:44 +0300

    main: init commit

---
+++ src/test/java/ru/yofujitsu/url_shortener/UrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yofujitsu.url_shortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
